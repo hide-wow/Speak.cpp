06e9e0fa1df1ffe7a72385d69ec8bb8b3511b0c7
HideLeOuf <78696725+hide-wow@users.noreply.github.com>
2022-06-10 22:58:21 +0200

    code is broken, just the beginning
    code is broken, just the beginning

---
+++ server/server.cpp
@@ -0,0 +1,78 @@
+#include <winsock2.h>
+#include <cstring>
+#include <iostream>
+#include <thread>
+#include <list>
+using namespace std;
+#pragma comment(lib, "ws2_32.lib")
+
+class Server
+{
+public:
+    thread threads[20];
+    SOCKET clients[20];
+
+    void clientHandle(SOCKET client)
+    {
+        string output;
+        char buffer[1024];
+        int recv_size;
+        while ((recv_size = recv(client, buffer, 1024, 0)) > 0)
+        {
+            for (int i = 0; i < recv_size; i++)
+            {
+                output += buffer[i];
+            }
+
+            broadcast(output);
+        }
+    }
+
+    void broadcast(string message)
+    {
+        for (int clientIndex = 0; clientIndex == sizeof(clients)/sizeof(*clients); clientIndex++)
+        {
+            if (clients[clientIndex] != INVALID_SOCKET)
+            {
+                send(clients[clientIndex], message.c_str(), message.length(), 0);
+            }
+        }
+    }
+
+    int main()
+    {
+        WSADATA WSAData;
+        SOCKET sock;
+        SOCKET csock;
+        SOCKADDR_IN sin;
+        SOCKADDR_IN csin;
+        WSAStartup(MAKEWORD(2,0), &WSAData);
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        sin.sin_addr.s_addr = INADDR_ANY;
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(23);
+        bind(sock, (SOCKADDR *)&sin, sizeof(sin));
+        listen(sock, 0);
+        while(1)
+        {
+            int sinsize = sizeof(csin);
+            if((csock = accept(sock, (SOCKADDR *)&csin, &sinsize)) != INVALID_SOCKET)
+            {
+                int arraySize = (sizeof(clients)/sizeof(*clients)) - 1;
+                if (arraySize > 20)
+                {
+                    string message = "Server is full";
+                    send(csock, message.c_str(), message.length(), 0);
+                    closesocket(csock);
+                }
+                else
+                {
+                    clients[arraySize+1] = csock;
+                    thread client (clientHandle, csock);
+                }
+            }
+        }
+        return 0;
+    }
+
+};
