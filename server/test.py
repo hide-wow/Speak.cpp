32cd4849b08631bacaede1ed125adfe8f86fb1c7
HideLeOuf <78696725+hide-wow@users.noreply.github.com>
2022-06-19 10:55:56 +0200

    Add files via upload

---
+++ server/test.py
@@ -0,0 +1,11 @@
+import socket
+host = ''
+port = 4448
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+while True:
+    client, address = server.accept()
+    print("+1 user")
+    client.send("successfully connected | welcome test user.".encode('utf-8'))
