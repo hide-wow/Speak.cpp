23db03a323952b69eab7cc74cd6b78abda89ec78
HideLeOuf <78696725+hide-wow@users.noreply.github.com>
2022-07-04 23:44:40 +0200

    restared the serverside code from 0

--- server/server.cpp
+++ server/server.cpp
@@ -1,77 +1,79 @@
+//SPEAK C++
+//BY hide-wow ON GITHUB
+
+//MADE TO LEARN C++ / C++ SOCKETS
+//USING WINSOCK
+
 #include <winsock2.h>
 #include <cstring>
 #include <iostream>
 #include <thread>
-#include <list>
 using namespace std;
+#pragma comment(lib, "ws2_32.lib")
 
-class Server
+int clients[20];
+int numClients;
+
+void broadcast(SOCKET sock, string message)
 {
-public:
-    thread threads[20];
-    SOCKET clients[20];
-
-    void clientHandle(SOCKET client)
+    for (int client = 0; client == numClients; client++)
     {
-        string output;
-        char buffer[1024];
-        int recv_size;
-        while ((recv_size = recv(client, buffer, 1024, 0)) > 0)
-        {
-            for (int i = 0; i < recv_size; i++)
-            {
-                output += buffer[i];
-            }
+        send(clients[numClients], message.c_str(), message.length(), 0);
+    }
+}
 
-            broadcast(output);
+void handle(SOCKET sock)
+{
+    while (1)
+    {
+        char clientMsg[1024];
+        recv(sock, clientMsg, 1024, 0);
+        broadcast(sock, string(clientMsg));
+    }
+}
+
+int main()
+{
+    // Setup var
+    WSADATA WSAData;
+    SOCKET sock;
+    SOCKET csock;
+    SOCKADDR_IN sin, csin;
+
+    // Setup winsock
+    WSAStartup(MAKEWORD(2, 0), &WSAData);
+
+    // Setup the socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(1337);
+
+    bind(sock, (SOCKADDR*)&sin, sizeof(sin));
+
+    /*
+    --SERVER SIDE SHEM--
+
+    SHEM -> 
+    1. Accept connexion
+       |-> 2. Thread him
+              |-> 3. Broadcast messages
+    
+    --SERVER SIDE SHEM END--
+    */
+
+    listen(sock, 0);
+    while (1)
+    {
+        int sinsize = sizeof(csin);
+        if ((csock = accept(sock, (SOCKADDR*)&csin, &sinsize)) != INVALID_SOCKET)
+        {
+            numClients++;
+            cout << numClients << endl;
+            clients[numClients] = csock;
+            thread client(handle, csock);
+            client.join();
         }
     }
-
-    void broadcast(string message)
-    {
-        for (int clientIndex = 0; clientIndex == sizeof(clients) / sizeof(*clients); clientIndex++)
-        {
-            if (clients[clientIndex] != INVALID_SOCKET)
-            {
-                send(clients[clientIndex], message.c_str(), message.length(), 0);
-            }
-        }
-    }
-
-    int main()
-    {
-        WSADATA WSAData;
-        SOCKET sock;
-        SOCKET csock;
-        SOCKADDR_IN sin;
-        SOCKADDR_IN csin;
-        WSAStartup(MAKEWORD(2, 0), &WSAData);
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-        sin.sin_addr.s_addr = INADDR_ANY;
-        sin.sin_family = AF_INET;
-        sin.sin_port = htons(23);
-        bind(sock, (SOCKADDR*)&sin, sizeof(sin));
-        listen(sock, 0);
-        while (1)
-        {
-            int sinsize = sizeof(csin);
-            if ((csock = accept(sock, (SOCKADDR*)&csin, &sinsize)) != INVALID_SOCKET)
-            {
-                int arraySize = (sizeof(clients) / sizeof(*clients)) - 1;
-                if (arraySize > 20)
-                {
-                    string message = "Server is full";
-                    send(csock, message.c_str(), message.length(), 0);
-                    closesocket(csock);
-                }
-                else
-                {
-                    clients[arraySize + 1] = csock;
-                    // thread client(&clientHandle, csock); -> Fucked part
-                }
-            }
-        }
-        return 0;
-    }
-
-};
+    return 0;
+}
